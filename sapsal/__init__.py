cf255b43e88a902bb4ba7f09cb67d1ff3839bbe0
kangdaeun <daeun.astro@gmail.com>
2025-11-21 17:29:38 +0100

    minor fix

--- sapsal/__init__.py
+++ sapsal/__init__.py
@@ -10,6 +10,7 @@ Created on Thu Jun  5 12:33:01 2025
 from . import models
 from . import FrEIA
 from . import tools
+from . import expander
 
 # for convinience
 from .cINN_config import read_config_from_file
@@ -17,4 +18,5 @@ from .data_loader import DataLoader
 
 __all__ = ["models", "FrEIA", "tools",
 			"read_config_from_file", "DataLoader",
+			"expander"
 			]
